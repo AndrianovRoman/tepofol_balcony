f288145c8d39a92dbce41905ad8b5c275a41a55a
Andrianov Roman <r_andrianov02@mail.ru>
2024-12-14 16:23:44 +0300

    create project

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+
+
+$('.material-image-items').not('.slick-initialized').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    responsive: [
+        // {
+        //     breakpoint: 1025,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 1,
+        //     }
+        // },
+    ]
+});
