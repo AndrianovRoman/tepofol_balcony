63414146a4496f11559189ca87c65ffda593bbcb
Andrianov Roman <r_andrianov02@mail.ru>
2024-12-17 13:17:24 +0300

    fix bug in safari #3

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
 
 
-$('.material-image-items').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    responsive: [
-        // {
-        //     breakpoint: 1025,
-        //     settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 1,
-        //     }
-        // },
-    ]
+$(document).ready(function(){
+    $('.material-image-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        responsive: [
+            // {
+            //     breakpoint: 1025,
+            //     settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 1,
+            //     }
+            // },
+        ]
+    });
 });
+
